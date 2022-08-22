06bed451c11266d909bfb3f5baec65878674bad9
Ramon Castillo Luna <ramon.castillo@optimo.design>
2022-08-22 23:04:47 +0200

    finalización

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe tu nombre, por favor:");
 string nombre = Console.ReadLine();
 Console.WriteLine("Hola, " + nombre);
 
